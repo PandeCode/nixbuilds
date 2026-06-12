f6ed4eb93935a0da2bd55c555a120727932d0815
PandeCode <pandeshawnbenjamin@gmail.com>
2026-06-12 11:40:17 -0400

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+this repo contains personal:
+- pkgs/derivations i've written to be built with ci and synced with my cachix
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "project templates";
+
+  nixConfig = {
+    trusted-users = ["root" "shawn"];
+    experimental-features = ["nix-command" "flakes" "pipe-operators"];
+    accept-flake-config = true;
+    show-trace = true;
+    auto-optimise-store = true;
+
+    # substituters = ["https://aseipp-nix-cache.freetls.fastly.net"];
+
+    extra-substituters = [
+      "https://charon.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "charon.cachix.org-1:epdetEs1ll8oi8DT8OG2jEA4whj3FDbqgPFvapEPbY8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nixutils = {
+      url = "github:pandecode/nixutils";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hermes = {
+      url = "github:pandecode/hermes";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+    libys = {
+      url = "github:pandecode/libys";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-alien = {
+      url = "github:thiagokokada/nix-alien";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    boomer.url = "github:nilp0inter/boomer";
+  };
+
+  outputs = {self, ...} @ inputs: {
+    nix.nixPath = ["nixpkgs=${self.inputs.nixpkgs}"];
+    packages = inputs.nixutils.lib.forAllSystems ((import ./packages.nix) (self // {}));
+  };
+}
